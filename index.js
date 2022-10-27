35992d96ef53ca8a688921a5a7da6575236af3c8
md-arif-khan <mdarifkhan8578@gmail.com>
2022-10-27 12:06:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express')
+var cors = require('cors')
+const courses= require ('./data/courses.json')
+const app=express()
+app.use(cors())
+const port =process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.send('server running ok')
+})
+app.get('/courses',(req,res)=>{
+    res.send(courses)
+})
+app.get('/courses/:id',(req,res)=>{
+    const id=req.params.id
+    const course=courses.find(course=>course.id==id)
+    res.send(course)
+})
+app.listen(port,()=>{
+    console.log('server running')
+})
